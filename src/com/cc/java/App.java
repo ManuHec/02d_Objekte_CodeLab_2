80bf1b83554ad569d4827206b081c9414e276043
Manuela <ellaheckenberger@gmail.com>
2021-12-03 16:24:17 +0100

    fertig

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -3,7 +3,34 @@ package com.cc.java;
 public class App {
 
 	public static void main(String[] args) {
-		
+
+		Mitarbeiter mitarbeiter1 = new Mitarbeiter("Mustermann", "Max", "Abteilungsleiter", "2000");
+
+		output("Vorname:" + " " + mitarbeiter1.getInfo("#vorname"));
+		output("Nachname:" + " " + mitarbeiter1.getInfo("#nachname"));
+		output("Funktion:" + " " + mitarbeiter1.getInfo("#funktion"));
+		output("Eintrittsjahr:" + " " + mitarbeiter1.getInfo("#eintrittsjahr"));
+
+		output("<--------------------------------->");
+
+		Mitarbeiter mitarbeiter2 = new Mitarbeiter("Freitag", "Lisa", "Personalleiterin", "2005");
+
+		output("Vorname:" + " " + mitarbeiter2.getInfo("#vorname"));
+		output("Nachname:" + " " + mitarbeiter2.getInfo("#nachname"));
+		output("Funktion:" + " " + mitarbeiter2.getInfo("#funktion"));
+		output("Eintrittsjahr:" + " " + mitarbeiter2.getInfo("#eintrittsjahr"));
+
+		output("<--------------------------------->");
+
+		Mitarbeiter mitarbeiter3 = new Mitarbeiter("Matern", "Maike", "Auszubildene", "2021");
+
+		output("Vorname:" + " " + mitarbeiter3.getInfo("#vorname"));
+		output("Nachname:" + " " + mitarbeiter3.getInfo("#nachname"));
+		output("Funktion:" + " " + mitarbeiter3.getInfo("#funktion"));
+		output("Eintrittsjahr:" + " " + mitarbeiter3.getInfo("#eintrittsjahr"));
+
+		output("<--------------------------------->");
+
 	}
 
 	private static void output(String outStr) {
---
+++ src/com/cc/java/Mitarbeiter.java
@@ -0,0 +1,49 @@
+package com.cc.java;
+
+public class Mitarbeiter {
+
+    public String nachname;
+    public String vorname;
+    public String funktion;
+    public String eintrittsjahr;
+
+    public Mitarbeiter(String nachname, String vorname, String funktion, String eintrittsjahr) {
+        this.nachname = nachname;
+        this.vorname = vorname;
+        this.funktion = funktion;
+        this.eintrittsjahr = eintrittsjahr;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public String getFunktion() {
+        return funktion;
+    }
+
+    public String getEintrittsjahr() {
+        return eintrittsjahr;
+    }
+
+    public String getInfo(String flag) {
+        switch (flag) {
+            case "#vorname":
+                return vorname;
+            case "#nachname":
+                return nachname;
+            case "#funktion":
+                return funktion;
+            case "#eintrittsjahr":
+                return eintrittsjahr;
+            default:
+                return "!2345fhj";
+        }
+
+    }
+
+}
